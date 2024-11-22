03172e9052929f7803711228a077a8cbe5de06db
vchandra22 <vincentchaandra@gmail.com>
2024-11-22 09:02:34 +0700

    feat: initialize new project springboot with dependecy spring web, postgresql, lombok, spring devtools

---
+++ src/main/java/com/warung_makan/aneka_rasa/AnekaRasaApplication.java
@@ -0,0 +1,13 @@
+package com.warung_makan.aneka_rasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnekaRasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnekaRasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warung_makan/aneka_rasa/AnekaRasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warung_makan.aneka_rasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnekaRasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
