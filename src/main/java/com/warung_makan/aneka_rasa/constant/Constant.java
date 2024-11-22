6a6c8a359131ba459923593cd1f0103a0d2508b3
vchandra22 <vincentchaandra@gmail.com>
2024-11-22 13:38:51 +0700

    feat: add constant menu_api & menu_table

---
+++ src/main/java/com/warung_makan/aneka_rasa/constant/Constant.java
@@ -0,0 +1,6 @@
+package com.warung_makan.aneka_rasa.constant;
+
+public class Constant {
+    public static final String MENU_API = "api/menus";
+    public static final String MENU_TABLE = "m_menu";
+}
