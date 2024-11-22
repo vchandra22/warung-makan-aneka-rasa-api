19ef08f7068c0068543fb17a0f24b800835914e4
vchandra22 <vincentchaandra@gmail.com>
2024-11-22 10:05:00 +0700

    feat: add simple /hello endpoint for testing

--- src/main/java/com/warung_makan/aneka_rasa/AnekaRasaApplication.java
+++ src/main/java/com/warung_makan/aneka_rasa/AnekaRasaApplication.java
@@ -2,12 +2,19 @@ package com.warung_makan.aneka_rasa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AnekaRasaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AnekaRasaApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
